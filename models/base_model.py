b4bbe344532b5cd820aae38166feb88830a396ce
imenmansouri93 <imennmansourii@gmail.com>
2022-10-11 15:33:24 +0100

    first commit

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,19 @@ class BaseModel:
         attr(updated_at): datetime - assign with the current datetime when an instance is created and it will be updated
         every time you change your object
         """
-        self.id = str(uuid4())
-        self.created_at =  datetime.now()
-        self.updated_at= datetime.now()
-
+        if len(kwargs)  != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or  key == "updated_at":
+                    obj = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, obj)
+                elif key == "__class__.__name__":
+                    setattr(self, key, value)
+        
+        else:
+            self.id = str(uuid4())
+            self.created_at =  datetime.now()
+            self.updated_at= datetime.now()
+            
     def __str__(self):
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
     
@@ -26,6 +35,7 @@ class BaseModel:
         
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
+        self.__dict__['id'] = self.id
         self.__dict__['__class__'] = self.__class__.__name__
         self.__dict__['updated_at'] = datetime.isoformat(self.__dict__['updated_at'])
         self.__dict__['created_at'] = datetime.isoformat(self.__dict__['created_at'])
