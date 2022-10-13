b593b58d54344d3f5ef5b37c5ebe908dd1f68b97
imenmansouri93 <imennmansourii@gmail.com>
2022-10-13 18:39:46 +0100

    new

--- models/place.py
+++ models/place.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""class Place"""
 from models.base_model import BaseModel
 
 class Place(BaseModel):
+    """inheritated class place from BaseModel"""
     city_id = ""
     user_id = ""
     name= ""
