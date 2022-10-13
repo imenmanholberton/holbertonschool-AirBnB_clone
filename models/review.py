02b66c7cb4194efd4d591d6cadd12f7c805a5863
imenmansouri93 <imennmansourii@gmail.com>
2022-10-13 18:40:38 +0100

    new

--- models/review.py
+++ models/review.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""class Reviex"""
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """inherited class Review from BaseModel"""
     place_id = ""
     user_id =""
     text=""
