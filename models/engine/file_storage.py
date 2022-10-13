5a98e5b4a8843c31f98f3ccea237d7ae7a1288ea
imenmansouri93 <imennmansourii@gmail.com>
2022-10-13 15:50:37 +0100

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,14 @@
 """"""
 import json
 from models.base_model import BaseModel
+from models import User
+from models import State
+from models import City
+from models import Amenity
+from models import Place
+from models import Review
+
+classe = {'BaseModel': BaseModel}
 
 
 class FileStorage:
@@ -10,24 +18,30 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """method all of file storage"""
         return (self.__objects)
 
     def new(self, obj):
+        """method new of file storage"""
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
+        """method save of file storage"""
         dict = {}
-        for key in self.__objects:
-            dict[key] = self.__objects[key].to_dict()
+        for key, value in self.__objects.items():
+            dict[key] = value.to_dict()
         with open(self.__file_path, 'w', ) as file:
             json.dump(dict, file)
     
     def reload(self):
+        """method reload of class reload"""
+        dict = {'BaseModel':BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
         try: 
-            with open(self.__file_path, 'r', encoding="UTF_8") as file:
-                n_dict = json.load(file)
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                n_dict = json.load(file.read())
                 for key, value in n_dict.items():
-                    attr = eval(value["__class__"])(**value)
+                    attr = eval(dict[value['__class__']](**value))
                     self.__objects[key] = attr
         except IOError:
             pass
