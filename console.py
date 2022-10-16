2f51187e5a7f2eb48cd87dd2fe6484a0c2b7bdbc
imenmansouri93 <imennmansourii@gmail.com>
2022-10-16 23:07:07 +0100

    new

--- console.py
+++ console.py
@@ -1,61 +1,142 @@
 #!/usr/bin/python3
-"""console"""
-import cmd
+"""console Airbnb project"""
+from datetime import datetime
 import models
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models import storage
-import shlex
 from shlex import split
+from models.user import User
+from models.place import  Place
+from models.state import State
+from models.city import City
+from models.amenity import  Amenity
+from models.review import Review
+from models import classes
 
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     
     """hbnb promt"""
     prompt = '(hbnb) '
-
-    def do_quit(self, arg) :
-        return  True
-
-    def do_EOF(self, arg):
-        print()
-        return True
+    clss = classes
 
     def emptyline(self):
         pass
 
-    def create (self, arg):
+    def do_create (self, arg):
         """create a new instance of BaseModel"""
         if arg == "":
             print("** class name missing **")
+            return
         List = arg.split()
+
         try:
             obj = eval(List[0])()
-            print(obj.id)
             obj.save()
+            print(obj.id)
         except:
             print("** class doesn't exist **")
-    def show(self, arg):
+    def do_show(self, arg):
         """show object by id"""
+        List = arg.split()
         if arg == "":
             print("** class name missing **")
-        List = parse(arg)
+            return
+        if len(List) == 1:
+            print("** instance id missing **")
+            return
         try:
-            obj = eval(List[0])()
+            eval(List[0])()
         except:
             print("** class doesn't exist **")
-        if len(List) == 1:
-            print("** instance idmisiing **")
-        check = False
-        for key, value in storage.all().items():
-            if key == "{}.{}".format(List[0],List[1]):
-                print(value)
-                check = True
-            if check == False:
-                print ("**no instance")
+        
+        obj = storage.all()
+        key = List[0]  +   "." +  List[1]
 
-def parse(arg):
-        return shlex.split(arg)
+        try:
+            value = obj[key]
+            print(value)
+        except KeyError:
+            print("** instance id missing **")
+
+    def do_destroy(self, arg):
+        """Delte an instance based on the class name"""
+        List = arg.split()
+        if arg == "":
+            print("** class name missing **")
+            return
+        if len[list] == 1:
+            print("** instance id missing **")
+        try:
+            eval(List[0])
+        except:
+            print("** class doesn't exist **")
+        obj = storage.all()
+        key = List[0] +  "." + List[1]
+
+        try:
+            del obj[key]
+        except:
+            print("** no instance found **")
+        storage.save()
+    
+    
+    def do_all(self, arg):
+        """Prints all string representation of all instances based
+        or not on the class name"""
+        List = arg.split()
+        if List[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            newList = []
+            for key, value in  storage.all().items():
+                if value.__class__.__name__== List[0]:
+                    newList += [value.__str__()]
+            print(newList)
+
+    def do_update(self, arg):
+        """pdate an objject by className and id, with attribute and value"""
+        if not arg:
+            print("** class name missing **")
+            return
+        Lists = arg.split(" ")
+        Objts =  storage.all()
+        if Lists[0] in self.clss:
+            if len(Lists) < 2:
+                print("** instance id missing **")
+                return
+            v = Lists[0] + "." + Lists[1]
+            if v not in Objts:
+                print("** no instance found **")
+            else:
+                obj = Objts[v]
+                attrbte  = ["id", "created_at", "updated_at"]
+                if obj:
+                    List = arg.split(" ")
+                    if len(List) < 3:
+                        print("** attribute name missing **")
+                    elif len(List) < 4:
+                        print("** value missing **")
+                    elif List[2] not in attrbte:
+                        obj.__dict__[List[2]] = List[3]
+                        obj.updated_at = datetime.now()
+                        storage.save()
+        else:
+            print("** class doesn't exist **")
+    
+    def do_quit(self, arg) :
+        """quit command"""
+        return  True
+
+    def do_EOF(self, arg):
+        """end of file"""
+        print()
+        return True
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
